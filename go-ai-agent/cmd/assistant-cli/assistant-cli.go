1eb83b29bc3fd65450ff8b369b1a6c4a9da63f3a
ZEY <1010642166@qq.com>
2026-07-29 08:05:27 +0800

    refactor: 将 json 格式输出检查提取为一个方法

--- go-ai-agent/cmd/assistant-cli/assistant-cli.go
+++ go-ai-agent/cmd/assistant-cli/assistant-cli.go
@@ -40,11 +40,10 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		if ans.Confidence > 1 || ans.Confidence < 0 {
-			fmt.Println("置信度不在0-1之间, 当前置信度:", ans.Confidence)
-		}
-		if ans.Intent != config.AgentQuestion && ans.Intent != config.RagQuestion && ans.Intent != config.ToolQuestion && ans.Intent != config.GeneralQuestion {
-			fmt.Println("意图不在指定范围内, 当前意图:", ans.Intent)
+
+		err = ans.IsValid()
+		if err != nil {
+			fmt.Println(" json 结果检查发现错误:", err)
 		}
 		fmt.Println("回答:", ans.Answer)
 		fmt.Println("============================== 分界线 ==============================")
--- go-ai-agent/internal/llm/output_schema.go
+++ go-ai-agent/internal/llm/output_schema.go
@@ -1,7 +1,24 @@
 package llm
 
+import (
+	"fmt"
+	"go-ai-agent/internal/config"
+)
+
 type IntentResult struct {
 	Intent     string  `json:"intent"`     // 用户意图
 	Answer     string  `json:"answer"`     // 回答
 	Confidence float64 `json:"confidence"` // 置信度
+}
+
+// IsValid 检查 json 格式输出内容是否正确
+// 检查置信度是否在 0 -- 1 之间; 另外还会检查 Intent 类型是否是四种规定类型之一
+func (res *IntentResult) IsValid() error {
+	if res.Confidence > 1 || res.Confidence < 0 {
+		return fmt.Errorf("置信度不在0-1之间, 当前置信度: %f", res.Confidence)
+	}
+	if res.Intent != config.AgentQuestion && res.Intent != config.RagQuestion && res.Intent != config.ToolQuestion && res.Intent != config.GeneralQuestion {
+		return fmt.Errorf("意图不在指定范围内, 当前意图: %s", res.Intent)
+	}
+	return nil
 }
